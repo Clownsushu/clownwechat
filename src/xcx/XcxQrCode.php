749d9b2316b446dadf7e2f06dddaee7d2f279bbe
15923719663 <812827345@qq.com>
2023-12-11 16:42:05 +0800

    Update XcxQrCode.php

--- src/xcx/XcxQrCode.php
+++ src/xcx/XcxQrCode.php
@@ -71,7 +71,7 @@ class XcxQrCode extends Base
         $result = curlPost($url, $params, 'json', true);
 
         if(!empty(json_decode($result, true))){
-           throw  new \Exception("请求失败, 返回内容: " . $result);
+            throw  new \Exception("请求失败, 返回内容: " . $result);
         }
         //保存路径为空直接返回
         if(empty($save_path)) return $result;
@@ -80,6 +80,41 @@ class XcxQrCode extends Base
         stringToImage($result, $save_path);
 
         return true;
+    }
 
+    /**
+     * 获取小程序二维码，适用于需要的码数量较少的业务场景。通过该接口生成的小程序码，永久有效，有数量限制, 参考https://developers.weixin.qq.com/miniprogram/dev/OpenApiDoc/qrcode-link/qr-code/createQRCode.html
+     * @param $path string 扫码进入的小程序页面路径，最大长度 128 个字符，不能为空
+     * @param $save_path string 保存图片的路径 不传直接返回二进制字符串
+     * @param $width int 二维码的宽度，单位 px。最小 280px，最大 1280px;默认是430
+     * @return array|mixed|true
+     * @throws \Exception
+     */
+    public function createQRCode($path = '', $save_path = '', $width = 430)
+    {
+        if(empty($path)){
+            throw new \Exception('请传入path参数, 扫码进入的小程序页面路径，最大长度 128 个字符，不能为空');
+        }
+        //获取请求地址
+        $url = $this->replaceUrl($this->config['createQRCode'], 'ACCESS_TOKEN', $this->access_token);
+        //组装参数
+        $params = [
+            'path' => $path,
+            'width' => $width,
+        ];
+
+        //发起请求
+        $result = curlPost($url, $params, 'json', true);
+
+        if(!empty(json_decode($result, true))){
+            throw  new \Exception("请求失败, 返回内容: " . $result);
+        }
+        //保存路径为空直接返回
+        if(empty($save_path)) return $result;
+
+        //字符串转图片并保存
+        stringToImage($result, $save_path);
+
+        return true;
     }
 }
